753dae2fdb7c33a088767724418054c0216bebe0
JelenaDJo <djokicj@yahoo.com>
2018-11-19 21:46:25 +0100

    Contact Form

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
+         <h3> Add Your Contact</h3>
+        <form @submit.prevent="addContact"> <!-- sprecava defoltnu funkciju relode-->
+            <label>First Name</label>
+            <input v-model="newContact.firstName" placeholder="First Name"/><br>
+
+            <label>Last Name</label>
+            <input v-model="newContact.lastName" placeholder="Last Name"/><br>
+
+            <label>Email</label>
+            <input v-model="newContact.email" placeholder="Email"/><br>
+            <button  type="submit"> ADD CONTACT</button>
+        </form>
+        
+
         <h3> Contact List</h3>
             <table border=1>
                 <thead>
@@ -17,23 +31,35 @@
                     </tr>
                 </tbody>
             </table>
-
+   
     </div>
 
 </template>
 
 <script>
 export default {
+    
     data(){
         return {
+            newContact: {
+                firstName: '',
+                lastName: '',
+                email: ''
+            },
             contacts: [
                 {firstName: 'John', lastName: 'Doe', email: 'example@example.com'},
                 {firstName: 'Jack', lastName: 'Smith', email: 'example@example.com'},
                 {firstName: 'Susan', lastName: 'Hart', email: 'example@example.com'},
                 {firstName: 'Sofi', lastName: 'Shepard', email: 'example@example.com'},
-                {firstName: 'Jasmin', lastName: 'Krev', email: 'example@example.com'}
+                {firstName: 'Jasmin', lastName: 'Krew', email: 'example@example.com'}
 
             ]
+        }
+    },
+    methods:{
+        addContact(){
+            this.contacts.push(this.newContact);
+            this.newContact={}
         }
     }
 }
