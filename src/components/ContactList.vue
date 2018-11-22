65273d71f6e6c9c912518dd734d665a5baf23c85
JelenaDJo <djokicj@yahoo.com>
2018-11-22 21:30:08 +0100

    ContactsTable dodavanje componente

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -14,34 +14,20 @@
         </form>
         
 
-        <h3> Contact List</h3>
-            <table border=1>
-                <thead>
-                    <th>Key</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th></th>
-                </thead>
-                <tbody>
-                    <tr v-for="(contact, index) in contacts" :key="index">
-                        <td>{{index + 1}}</td>
-                        <td>{{contact.firstName}}</td>
-                        <td>{{contact.lastName}}</td>
-                        <td>{{contact.email}}</td>
-                        <td>
-                        <button class="btn btn-danger" @click="removeContact(contact)"> Remove </button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-   
+        <ContactsTable  :contactsList="contacts"/>
+            
     </div>
 
 </template>
 
 <script>
+
+import ContactsTable from './ContactsTable'
+
 export default {
+    components: {
+        ContactsTable
+    },
     
     data(){
         return {
---
+++ src/components/ContactsTable.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <h1> Contact table</h1>
+ <table border=1>
+                <thead>
+                    <th>Key</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <tr v-for="(contact, index) in contactsList" :key="index">
+                        <td>{{index + 1}}</td>
+                        <td>{{contact.firstName}}</td>
+                        <td>{{contact.lastName}}</td>
+                        <td>{{contact.email}}</td>
+                        <td>
+                        <button class="btn btn-danger" @click="removeContact(contact)"> Remove </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+</div>
+
+
+</template>
+
+
+<script>
+export default {
+    props: ['contactsList']
+}
+</script>
+
